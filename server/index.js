fed351d0eb804d0a5bf69e87774cc483366b1520
kampkelly <andela@Andelas-MacBook-Pro-2.local>
2019-04-19 23:35:03 +0100

    ch(setup-project): setup the project
    - add eslint and airbnb style guide
    - add babel
    - add sequelize to interact with database
    
    [Finishes #]

---
+++ server/index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import logger from 'morgan';
+import errorhandler from 'errorhandler';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const accessLogStream = fs.createWriteStream(`${__dirname}/access.log`, { flags: 'a' });
+const isProduction = process.env.NODE_ENV === 'production';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev', { stream: accessLogStream }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+// catch 404 and forward to error handler
+// eslint-disable-next-line
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  // eslint-disable-next-line
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Shopmate listeninnng on port ${process.env.PORT || 3000}`);
+});
+
+export default app;
