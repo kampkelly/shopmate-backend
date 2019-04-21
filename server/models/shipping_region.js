f885b7399b6325708ebd444de29b79c9cbb5acad
kampkelly <andela@Andelas-MacBook-Pro-2.local>
2019-04-21 21:10:11 +0100

    ft(register-customer): register a customer
    - register a customer
    - write tests
    
    [Finishes]

---
+++ server/models/shipping_region.js
@@ -0,0 +1,22 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const ShippingRegion = sequelize.define('ShippingRegion', {
+    shipping_region_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    shipping_region_: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    }
+  }, { freezeTableName: true, tableName: 'shipping_region', timestamps: false });
+  ShippingRegion.associate = (models) => {
+    // associations can be defined here
+    ShippingRegion.belongsTo(models.Customer, {
+      targetKey: 'customer_id'
+    });
+  };
+  return ShippingRegion;
+};
--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import ProductController from '../../controllers/productController';
 
-
 const router = express.Router();
 router.get('/products', ProductController.viewAllProducts);
 router.get('/products/:productId', ProductController.viewSingleProduct);
