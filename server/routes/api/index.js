b12b2814d6f4c634cd10e6914892255ecb24b874
kampkelly <oghenerunor@Andelas-MacBook-Pro-2.local>
2019-04-23 18:33:41 +0100

    ft(facebook-login): login with facebook
    - login with facebook
    - write test
    
    [Finishes]

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import product from './product';
 import customer from './customer';
 import shoppingCart from './shoppingCart';
+import socialLogin from './socialLogin';
 
 const router = express.Router();
 
 router.use('/', product);
 router.use('/', customer);
 router.use('/', shoppingCart);
+router.use('/', socialLogin);
 
 export default router;
