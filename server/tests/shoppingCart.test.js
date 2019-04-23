ad5c41e4af8043101c823d2ca16051e282764277
kampkelly <oghenerunor@Andelas-MacBook-Pro-2.local>
2019-04-23 10:47:41 +0100

    ft(add-product-to-cart): add product to cart
    - add product to cart
    - write test

--- server/tests/shoppingCart.test.js
+++ server/tests/shoppingCart.test.js
@@ -4,6 +4,25 @@ import 'babel-polyfill';
 import app from '../index';
 
 chai.use(chaiHttp);
+const cartDetails = {
+  cart_id: '',
+  product_id: 1,
+  attributes: 'color:blue'
+};
+
+const cartDetailsUpdate = {
+  cart_id: '',
+  product_id: 1,
+  attributes: 'size:m'
+};
+
+const cartDetailsWithInvalidProductId = {
+  cart_id: '',
+  product_id: 1001,
+  attributes: 'color'
+};
+let cartQuantity = 0;
+let cartLength = 0;
 
 describe('Shopping Cart', () => {
   it('Should generate a unique id', (done) => {
@@ -14,13 +33,69 @@ describe('Shopping Cart', () => {
         expect(res.body).to.be.an('object');
         expect(res.body.cart_id).to.be.a('string');
         expect(res.body.cart_id).to.have.lengthOf(18);
+        cartDetails.cart_id = res.body.cart_id;
+        cartDetailsUpdate.cart_id = res.body.cart_id;
+        done();
+      });
+  });
+
+  it('Should add product to cart', (done) => {
+    chai.request(app)
+      .post('/shoppingcart/add')
+      .send(cartDetails)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(1);
+        expect(res.body[0].attributes).to.equal('color:blue');
+        cartQuantity = res.body[0].quantity;
+        cartLength += 1;
+        done();
+      });
+  });
+
+  it('Should update product quantity in cart if product exists already with cart id and attribute', (done) => {
+    chai.request(app)
+      .post('/shoppingcart/add')
+      .send(cartDetails)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body[0].quantity).to.equal(cartQuantity + 1);
+        expect(res.body).to.have.lengthOf(cartLength);
+        expect(res.body[0].attributes).to.equal('color:blue');
+        done();
+      });
+  });
+
+  it('Should add new product in cart cart id and attribute or attribute differs', (done) => {
+    chai.request(app)
+      .post('/shoppingcart/add')
+      .send(cartDetailsUpdate)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(cartLength + 1);
+        expect(res.body[1].attributes).to.equal('size:m');
+        done();
+      });
+  });
+
+  it('Should not add product to cart if product does not exist', (done) => {
+    chai.request(app)
+      .post('/shoppingcart/add')
+      .send(cartDetailsWithInvalidProductId)
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.cart).to.be.an('array');
+        expect(res.body.message).to.equal('Product cannot be found');
         done();
       });
   });
 
   it('Should get products in cart', (done) => {
     chai.request(app)
-      .get('/shoppingcart/1cc')
+      .get(`/shoppingcart/${cartDetails.cart_id}`)
       .end((err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array');
