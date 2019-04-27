eb21a395343602f7836e4e5ea0b8ab0cf1b17b13
kampkelly <oghenerunor@Andelas-MacBook-Pro-3.local>
2019-04-27 05:38:12 -0700

    ch(push-to-heroku): push to heroku
    - add procfile
    - modify npm script
    - update .env.sample file
    
    [Finishes]

--- server/models/customer.js
+++ server/models/customer.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
       unique: true
     },
     password: {
-      type: DataTypes.STRING(50),
+      type: DataTypes.STRING(100),
       allowNull: false
     },
     credit_card: {
