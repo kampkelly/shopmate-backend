c5906c00ee8468ee301f6355c5946db754d29620
kampkelly <oghenerunor@Andelas-MacBook-Pro-3.local>
2019-04-27 12:38:14 -0700

    ft(add-redis-for-caching): use redis for caching
    - add redis for caching data in products and shopping cart
    
    [Finishes]

--- server/middlewares/productValidator.js
+++ server/middlewares/productValidator.js
@@ -5,17 +5,24 @@ import errorResponse from '../helpers/errorResponse';
 export default {
   validateQueryParams(req, res, next) {
     const {
-      description_length: descriptionLength, limit, page
+      description_length: descriptionLength, limit, page, query_string: queryString
     } = req.query;
     const errors = [];
+    req.cacheKeyPrefix = '';
     if (descriptionLength) {
+      req.cacheKeyPrefix = `${req.cacheKeyPrefix}-d${descriptionLength}`;
       if (isNaN(descriptionLength)) errors.push('Description length must be a number');
     }
     if (limit) {
+      req.cacheKeyPrefix = `${req.cacheKeyPrefix}-l${limit}`;
       if (isNaN(limit)) errors.push('Limit must be a number');
     }
     if (page) {
+      req.cacheKeyPrefix = `${req.cacheKeyPrefix}-p${page}`;
       if (isNaN(page)) errors.push('Page must be a number');
+    }
+    if (queryString) {
+      req.cacheKeyPrefix = `${req.cacheKeyPrefix}-q${queryString}`;
     }
     if (errors.length) {
       return res.status(400).json(errorResponse(req, res, 400, 'USR_04', errors, ''));
--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import ProductController from '../../controllers/productController';
 import productValidator from '../../middlewares/productValidator';
+import productCaching from '../../middlewares/productCaching';
 
 const router = express.Router();
-router.get('/products', productValidator.validateQueryParams, ProductController.viewAllProducts);
-router.get('/products/search', productValidator.validateQueryParams, ProductController.searchProducts);
+router.get('/products', productValidator.validateQueryParams, productCaching.allProductsCaching, ProductController.viewAllProducts);
+router.get('/products/search', productValidator.validateQueryParams, productCaching.searchProductsCaching, ProductController.searchProducts);
 router.get('/products/:productId', ProductController.viewSingleProduct);
-router.get('/products/inCategory/:categoryId', productValidator.validateQueryParams, ProductController.getProductsInCategory);
-router.get('/products/inDepartment/:departmentId', productValidator.validateQueryParams, ProductController.getProductsInDepartment);
+router.get('/products/inCategory/:categoryId', productValidator.validateQueryParams, productCaching.categoryProductsCaching, ProductController.getProductsInCategory);
+router.get('/products/inDepartment/:departmentId', productValidator.validateQueryParams, productCaching.departmentProductsCaching, ProductController.getProductsInDepartment);
 
 export default router;
