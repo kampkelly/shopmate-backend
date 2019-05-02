29df3fdb148d5ea9644b5505eb81754f3aed16bd
kampkelly <oghenerunor@Andelas-MacBook-Pro-3.local>
2019-05-02 17:57:31 +0100

    ft(retrieve-shipping-regions): retrieve shipping regions
    - retrieve shipping regions
    - write test
    
    [Finishes]

--- server/database/seeders/20190428092715-default-shipping_regions.js
+++ server/database/seeders/20190428092715-default-shipping_regions.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   up: queryInterface => queryInterface.bulkInsert('shipping_region', [
-    { shipping_region_id: 1, shipping_region: 'Please select' },
-    { shipping_region_id: 2, shipping_region: 'US / Canade' },
+    { shipping_region_id: 1, shipping_region: 'Please Select' },
+    { shipping_region_id: 2, shipping_region: 'US / Canada' },
     { shipping_region_id: 3, shipping_region: 'Europe' },
     { shipping_region_id: 4, shipping_region: 'Rest of World' }
   ], {}),
